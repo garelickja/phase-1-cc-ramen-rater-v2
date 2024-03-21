0849b340370423608db587b107ec64869cd591bb
garelickja <138186599+garelickja@users.noreply.github.com>
2024-03-21 15:54:49 -0500

    ramen rater

--- src/index.js
+++ src/index.js
@@ -1,29 +1,64 @@
-// index.js
+document.addEventListener("DOMContentLoaded", main);
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
-
-const addSubmitListener = () => {
-  // Add code
+function main() {
+  displayRamens();
+  addSubmitListener();
 }
 
 const displayRamens = () => {
-  // Add code
+  fetch("http://localhost:3000/ramens")
+    .then((res) => res.json())
+    .then((ramens) => {
+      const ramenMenu = document.getElementById("ramen-menu");
+      ramens.forEach((ramen) => {
+        const ramenImage = document.createElement("img");
+        ramenImage.src = ramen.image;
+        ramenImage.addEventListener("click", () => handleClick(ramen));
+        ramenMenu.appendChild(ramenImage);
+      });
+    });
 };
 
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+const handleClick = (ramen) => {
+  const detailImage = document.querySelector(".detail-image");
+  const name = document.querySelector(".name");
+  const restaurant = document.querySelector(".restaurant");
+  const ratingDisplay = document.getElementById("rating-display");
+  const commentDisplay = document.getElementById("comment-display");
 
-main()
+  detailImage.src = ramen.image;
+  name.textContent = ramen.name;
+  restaurant.textContent = ramen.restaurant;
+  ratingDisplay.textContent = ramen.rating;
+  commentDisplay.textContent = ramen.comment;
+};
 
-// Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
+const addSubmitListener = () => {
+  const form = document.getElementById("new-ramen");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newName = document.getElementById("new-name").value;
+    const newRestaurant = document.getElementById("new-restaurant").value;
+    const newImage = document.getElementById("new-image").value;
+    const newRating = document.getElementById("new-rating").value;
+    const newComment = document.getElementById("new-comment").value;
+
+    const newRamen = {
+      newName,
+      newRestaurant,
+      newImage,
+      newRating,
+      newComment,
+    };
+
+    const ramenMenu = document.getElementById("ramen-menu");
+    const ramenImage = document.createElement("img");
+    ramenImage.src = newRamen.image;
+    ramenImage.addEventListener("click", () => handleClick(newRamen));
+    ramenMenu.appendChild(ramenImage);
+
+    handleClick(newRamen);
+
+    form.reset();
+  });
 };
